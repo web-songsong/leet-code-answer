b927811690360d35cc867c7e8949c8f37165e87c
charmingsong <web_songsong@163.com>
2019-10-21 21:50:30 +0800

    test 10

--- 10-isMatch.js
+++ 10-isMatch.js
@@ -3,38 +3,10 @@
  * @param {string} p
  * @return {boolean}
  */
-s = "aaaaaaaaaabbbbb"
-p = ".*"
 var isMatch = function (s, p) {
-
-  let index = 0
-
-  const reg = /(\w|.)(\*)*/g
-
-  // let ident, flag = false
-
-  let reg_info = reg.exec(p)
-
-  function info_format() {
-    
-    if (!reg_info) return true
-    
-    
-    ident = reg_info[0]
-    flag = reg_info.length > 2
-    
-    if (flag){}
-  }
-
-  while (index === s.length - 1) {
-    if (!info_format(s[index])) {
-      return false
-    }
-  }
-  return true
+  let len_s = s.length -1
+  let len_p = p.length -1
+  
 }
-// console.log(isMatch(s, p))
-
-a = "aab"
-b = "c*a*b"
-console.log(isMatch(a, b))
+s = "aab"
+p = "c*a*b" 
