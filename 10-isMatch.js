490e9cd09ae56731cdc545c3a4bc2b4d9b5401a2
charmingsong <web_songsong@163.com>
2019-10-22 15:15:44 +0800

    10

--- 10-isMatch.js
+++ 10-isMatch.js
@@ -4,9 +4,22 @@
  * @return {boolean}
  */
 var isMatch = function (s, p) {
-  let len_s = s.length -1
-  let len_p = p.length -1
-  
+
+  function match(ins, inp) {
+    if (inp === -1) return (ins === -1)
+    if (p[inp] === '*') {
+      if (ins > -1 && (p[inp - 1] === s[ins] || p[inp - 1] === '.')) {
+        if (match(ins - 1, inp)) return true
+      }
+      return match(ins - 1, inp - 2)
+    }
+    if (ins > -1 && (p[inp] === s[ins] || p[inp] === '.')) return match(ins - 1, inp - 1)
+    return false
+  }
+
+  return match(s.length - 1, p.length - 1)
+
 }
-s = "aab"
-p = "c*a*b" 
+s = "mississippi"
+p = "mis*is*p*."
+console.log(isMatch(s, p))
